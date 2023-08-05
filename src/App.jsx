e396df1b052fc8addcb7f1fae378fcb37facc290
luciano-meloclaps <meloclapsluciano@gmail.com>
2023-08-05 20:08:59 -0300

    update and finish sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Sidebar from "./components/shared/Sidebar";
 
 function App() {
   return (
-    <div className="bg-gray-800 w-full min-h-screen">
+    <div className="bg-[#262626] w-full min-h-screen">
       <Sidebar></Sidebar>
     </div>
   );
--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,29 +1,91 @@
 import React from "react";
-import { RiHome3Line } from "react-icons/ri";
-import { BiSolidGame } from "react-icons/bi";
+import {
+  BiSolidGame,
+  BiSolidBadgeDollar,
+  BiSolidHome,
+  BiSolidChat,
+  BiSolidTrashAlt,
+  BiSolidWrench,
+  BiSolidBell,
+  BiSolidLogIn,
+} from "react-icons/bi";
 
 const Sidebar = () => {
   return (
-    <div className="bg-gray-950 fixed left-0 top-0 w-28 h-full">
-      <ul className="pl-5">
-        <li>
-          <h1 className="text-yellow-300 text-5xl uppercase font-bold text-center my-5 ">
-            <a href="#" className="bg-yellow-300">
-              <BiSolidGame></BiSolidGame>
+    <div className="bg-[#000000] fixed left-0 top-0 w-28 h-full flex flex-col justify-between">
+      <div> {/*Sidebar Items*/}
+        <ul className="pl-5">
+          <li> {/*Icon Logo*/}
+            {" "}
+            <h1 className="text-[#F4CF2A] text-5xl uppercase font-bold my-8 ">
+              <a href="#" className="bg-yellow-300">
+                <BiSolidGame></BiSolidGame>
+              </a>
+            </h1>
+          </li>
+          <li className="bg-[#262626] p-4 rounded-tl-2xl rounded-bl-2xl"> {/*Icon Home*/}
+            <a
+              href="#"
+              className="bg-[#F4CF2A] p-4  rounded-xl flex justify-center"
+            >
+              <BiSolidHome className="text-2xl"></BiSolidHome>
             </a>
-          </h1>
-        </li>
-        <li className="bg-gray-800 p-4 rounded-tl-2xl rounded-bl-2xl ">
-          <a href="#" className="bg-yellow-300 p-4 block rounded-xl">
-            <RiHome3Line className="text-xl"></RiHome3Line>
-          </a>
-        </li>
-        <li className="p-4 rounded-tl-2xl rounded-bl-2xl ">
-          <a href="#" className="p-4 block rounded-xl text-yellow-300">
-            <RiHome3Line className="text-xl"></RiHome3Line>
-          </a>
-        </li>
-      </ul>
+          </li>
+          <li className="hover:bg-[#262626] p-4 rounded-tl-2xl rounded-bl-2xl group transition-colors">{/*Icon Dollar*/}
+            <a
+              href="#"
+              className="group-hover:bg-[#F4CF2A] p-4 flex justify-center rounded-xl text-[#F4CF2A] group-hover:text-[#000000] transition-colors "
+            >
+              <BiSolidBadgeDollar className="text-2xl"></BiSolidBadgeDollar>
+            </a>
+          </li>
+          <li className="hover:bg-[#262626] p-4 rounded-tl-2xl rounded-bl-2xl group transition-colors">{/*Icon Chat*/}
+            <a
+              href="#"
+              className="group-hover:bg-[#F4CF2A] p-4 flex justify-center rounded-xl text-[#F4CF2A] group-hover:text-[#000000] transition-colors "
+            >
+              <BiSolidChat className="text-2xl"></BiSolidChat>
+            </a>
+          </li>
+          <li className="hover:bg-[#262626] p-4 rounded-tl-2xl rounded-bl-2xl group transition-colors">{/*Icon Trash*/}
+            <a
+              href="#"
+              className="group-hover:bg-[#F4CF2A] p-4 flex justify-center rounded-xl text-[#F4CF2A] group-hover:text-[#000000] transition-colors "
+            >
+              <BiSolidTrashAlt className="text-2xl"></BiSolidTrashAlt>
+            </a>
+          </li>
+          <li className="hover:bg-[#262626] p-4 rounded-tl-2xl rounded-bl-2xl group transition-colors"> {/*Icon Notification*/}
+            <a
+              href="#"
+              className="group-hover:bg-[#F4CF2A] p-4 flex justify-center rounded-xl text-[#F4CF2A] group-hover:text-[#000000] transition-colors "
+            >
+              <BiSolidBell className="text-2xl"></BiSolidBell>
+            </a>
+          </li>
+          <li className="hover:bg-[#262626] p-4 rounded-tl-2xl rounded-bl-2xl group transition-colors"> {/*Icon Settings*/}
+            <a
+              href="#"
+              className="group-hover:bg-[#F4CF2A] p-4 flex justify-center rounded-xl text-[#F4CF2A] group-hover:text-[#000000] transition-colors "
+            >
+              <BiSolidWrench className="text-2xl"></BiSolidWrench>
+            </a>
+          </li>
+        </ul>
+      </div>
+      
+      <div> {/*Log out*/}
+        <ul className="pl-4">
+          <li className="hover:bg-[#262626] p-4 rounded-tl-2xl rounded-bl-2xl group transition-colors"> {/*Icon Log out*/}
+            <a
+              href="#"
+              className="group-hover:bg-[#F4CF2A] p-4 flex justify-center rounded-xl text-[#F4CF2A] group-hover:text-[#000000] transition-colors "
+            >
+              <BiSolidLogIn className="text-2xl"></BiSolidLogIn>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
