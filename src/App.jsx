d1870b10b85f28867fd621f25df5a40746e06b60
luciano-meloclaps <meloclapsluciano@gmail.com>
2023-08-06 12:11:27 -0300

    update Sidebar, create Sidebar movil

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
+//Components
+import { useState } from "react";
 import Sidebar from "./components/shared/Sidebar";
+//Libraries
+import {
+  BiSolidHome,
+  BiSolidGrid,
+  BiSolidUser,
+  BiSolidBadgeDollar,
+  BiSolidXCircle,
+} from "react-icons/bi";
 
 function App() {
+  const [showMenu, setShowMenu] = useState(false);
+  const [showOrder, setShowOrder] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className="bg-[#262626] w-full min-h-screen">
-      <Sidebar></Sidebar>
+      <Sidebar showMenu={showMenu} />
+      {/*Menu Celphone*/}
+      <nav className="bg-[#F4CF2A] lg:hidden fixed w-full bottom-0 left-0 text-3xl py-4 px-12 flex items-center justify-between rounded-tl-xl rounded-tr-xl">
+        <button>
+          <BiSolidHome></BiSolidHome>
+        </button>
+        <button>
+          <BiSolidUser></BiSolidUser>
+        </button>
+        <button>
+          <BiSolidBadgeDollar></BiSolidBadgeDollar>
+        </button>
+        <button onClick={toggleMenu}>
+          {showMenu ? <BiSolidXCircle /> : <BiSolidGrid />}
+          
+        </button>
+      </nav>
     </div>
   );
 }
--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {
-  BiSolidGame,
   BiSolidBadgeDollar,
   BiSolidHome,
   BiSolidChat,
@@ -10,20 +9,32 @@ import {
   BiSolidLogIn,
 } from "react-icons/bi";
 
-const Sidebar = () => {
+const Sidebar = (props) => {
+  const { showMenu } = props;
+
   return (
-    <div className="bg-[#000000] fixed left-0 top-0 w-28 h-full flex flex-col justify-between">
-      <div> {/*Sidebar Items*/}
+    <div
+      className={`bg-[#000000] fixed lg:left-0 top-0 w-28 h-full flex flex-col justify-between py-6 rounded-tr-2xl
+      rounded-br-2xl z-50 -left-full transition-all ${
+        showMenu ? "left-0" : "-left-full"
+      }`}
+    >
+      <div>
+        {/*Sidebar Items*/}
         <ul className="pl-5">
-          <li> {/*Icon Logo*/}
-            {" "}
-            <h1 className="text-[#F4CF2A] text-5xl uppercase font-bold my-8 ">
-              <a href="#" className="bg-yellow-300">
-                <BiSolidGame></BiSolidGame>
+          <li>
+            {/*Icon Logo*/}
+            <h1 className="text-[#F4CF2A] my-8 flex justify-items-center">
+              <a href="#">
+                <img
+                  src="vecteezy_burger-3d-illustration_13167062_30.png"
+                  style={{ width: "85px" }}
+                ></img>
               </a>
             </h1>
           </li>
-          <li className="bg-[#262626] p-4 rounded-tl-2xl rounded-bl-2xl"> {/*Icon Home*/}
+          <li className="bg-[#262626] p-4 rounded-tl-2xl rounded-bl-2xl">
+            {/*Icon Home*/}
             <a
               href="#"
               className="bg-[#F4CF2A] p-4  rounded-xl flex justify-center"
@@ -31,7 +42,8 @@ const Sidebar = () => {
               <BiSolidHome className="text-2xl"></BiSolidHome>
             </a>
           </li>
-          <li className="hover:bg-[#262626] p-4 rounded-tl-2xl rounded-bl-2xl group transition-colors">{/*Icon Dollar*/}
+          <li className="hover:bg-[#262626] p-4 rounded-tl-2xl rounded-bl-2xl group transition-colors">
+            {/*Icon Dollar*/}
             <a
               href="#"
               className="group-hover:bg-[#F4CF2A] p-4 flex justify-center rounded-xl text-[#F4CF2A] group-hover:text-[#000000] transition-colors "
@@ -39,7 +51,8 @@ const Sidebar = () => {
               <BiSolidBadgeDollar className="text-2xl"></BiSolidBadgeDollar>
             </a>
           </li>
-          <li className="hover:bg-[#262626] p-4 rounded-tl-2xl rounded-bl-2xl group transition-colors">{/*Icon Chat*/}
+          <li className="hover:bg-[#262626] p-4 rounded-tl-2xl rounded-bl-2xl group transition-colors">
+            {/*Icon Chat*/}
             <a
               href="#"
               className="group-hover:bg-[#F4CF2A] p-4 flex justify-center rounded-xl text-[#F4CF2A] group-hover:text-[#000000] transition-colors "
@@ -47,7 +60,8 @@ const Sidebar = () => {
               <BiSolidChat className="text-2xl"></BiSolidChat>
             </a>
           </li>
-          <li className="hover:bg-[#262626] p-4 rounded-tl-2xl rounded-bl-2xl group transition-colors">{/*Icon Trash*/}
+          <li className="hover:bg-[#262626] p-4 rounded-tl-2xl rounded-bl-2xl group transition-colors">
+            {/*Icon Trash*/}
             <a
               href="#"
               className="group-hover:bg-[#F4CF2A] p-4 flex justify-center rounded-xl text-[#F4CF2A] group-hover:text-[#000000] transition-colors "
@@ -55,7 +69,9 @@ const Sidebar = () => {
               <BiSolidTrashAlt className="text-2xl"></BiSolidTrashAlt>
             </a>
           </li>
-          <li className="hover:bg-[#262626] p-4 rounded-tl-2xl rounded-bl-2xl group transition-colors"> {/*Icon Notification*/}
+          <li className="hover:bg-[#262626] p-4 rounded-tl-2xl rounded-bl-2xl group transition-colors">
+            {" "}
+            {/*Icon Notification*/}
             <a
               href="#"
               className="group-hover:bg-[#F4CF2A] p-4 flex justify-center rounded-xl text-[#F4CF2A] group-hover:text-[#000000] transition-colors "
@@ -63,7 +79,9 @@ const Sidebar = () => {
               <BiSolidBell className="text-2xl"></BiSolidBell>
             </a>
           </li>
-          <li className="hover:bg-[#262626] p-4 rounded-tl-2xl rounded-bl-2xl group transition-colors"> {/*Icon Settings*/}
+          <li className="hover:bg-[#262626] p-4 rounded-tl-2xl rounded-bl-2xl group transition-colors">
+            {" "}
+            {/*Icon Settings*/}
             <a
               href="#"
               className="group-hover:bg-[#F4CF2A] p-4 flex justify-center rounded-xl text-[#F4CF2A] group-hover:text-[#000000] transition-colors "
@@ -73,10 +91,14 @@ const Sidebar = () => {
           </li>
         </ul>
       </div>
-      
-      <div> {/*Log out*/}
-        <ul className="pl-4">
-          <li className="hover:bg-[#262626] p-4 rounded-tl-2xl rounded-bl-2xl group transition-colors"> {/*Icon Log out*/}
+
+      <div>
+        {" "}
+        {/*Log out*/}
+        <ul className="pl-5 py-10">
+          <li className="hover:bg-[#262626] p-4 rounded-tl-2xl rounded-bl-2xl group transition-colors">
+            {" "}
+            {/*Icon Log out*/}
             <a
               href="#"
               className="group-hover:bg-[#F4CF2A] p-4 flex justify-center rounded-xl text-[#F4CF2A] group-hover:text-[#000000] transition-colors "
